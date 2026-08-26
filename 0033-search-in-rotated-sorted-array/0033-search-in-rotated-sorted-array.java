86092cfd0dba4d5e596901e396c71a9bdf5c5466
AKSHAT GUPTA <133819077+Akshat-Gupta-2005@users.noreply.github.com>
2026-08-26 14:25:46 +0530

    Time: 0 ms (100%), Space: 43.9 MB (28.21%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -18,22 +18,41 @@ class Solution {
 
         // return  -1;
 
-        int l = 0 ; 
-        int r = nums.length-1;
+        // int l = 0 ; 
+        // int r = nums.length-1;
+
+        // while(l<=r){
+        //     int mid = l+(r-l)/2;
+
+        //     if (nums[mid] == target) return mid;
+
+        //     if (nums[l] <= nums[mid]){
+        //         if (nums[l]<=target && target <= nums[mid]) r= mid-1;
+        //         else l = mid+1;
+        //     }
+        //     if (nums[mid] <= nums[r]){
+        //         if (nums[mid]<=target && target<=nums[r]) l= mid+1;
+        //         else r = mid-1;
+        //     }
+        // }
+
+        // return -1;
+
+        int l = 0 ;
+        int r = nums.length - 1;
 
         while(l<=r){
-            int mid = l+(r-l)/2;
+            int mid = l + (r-l)/2;
 
             if (nums[mid] == target) return mid;
 
             if (nums[l] <= nums[mid]){
-                if (nums[l]<=target && target <= nums[mid]) r= mid-1;
-                else l = mid+1;
-            }
-            if (nums[mid] <= nums[r]){
-                if (nums[mid]<=target && target<=nums[r]) l= mid+1;
-                else r = mid-1;
-            }
+                if (nums[l] <= target && target <= nums[mid]) r = mid -1;
+                else l = mid +1;
+            }else if (nums[mid] <= nums[r]){
+                if (nums[mid] <= target && target <= nums[r]) l = mid +1;
+                else r = mid -1;
+            } 
         }
 
         return -1;
