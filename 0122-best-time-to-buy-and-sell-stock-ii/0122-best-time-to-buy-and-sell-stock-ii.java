494f3d7eb698b43ca42cf980c4302017277f3abb
AKSHAT GUPTA <133819077+Akshat-Gupta-2005@users.noreply.github.com>
2026-08-27 12:27:08 +0530

    Time: 2 ms (16.94%), Space: 46.5 MB (31.72%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int rec(int i , boolean buy , int [] prices,int [][] dp ){
+        if (i == prices.length) return 0;
+        if (dp[i][buy?1:0] != 0) return dp[i][buy?1:0];
+        if (buy){
+            return dp[i][1] = Math.max(-prices[i] + rec(i+1,false,prices,dp) , 0 + rec(i+1,true,prices,dp));
+        }else{
+            return dp[i][0] = Math.max(prices[i] + rec(i+1,true,prices,dp) , 0 + rec(i+1,false,prices,dp));
+        }
+    }
+    public int maxProfit(int[] prices) {
+        // int [][] dp = new int[prices.length + 1][2];
+        // rec(0 , true, prices , dp);
+        // return dp[0][1];
+        int [][] dp = new int[prices.length + 1][2];
+        for (int i = prices.length-1; i >=0 ; i--){
+            dp[i][1] = Math.max(-prices[i] + dp[i+1][0] , 0 + dp[i+1][1]);
+            dp[i][0] = Math.max(prices[i] + dp[i+1][1] , 0 + dp[i+1][0]);
+        }
+        return dp[0][1];
+
+
+    }
+}
